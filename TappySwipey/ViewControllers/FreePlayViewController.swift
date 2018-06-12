ce2b57ec5e0c12a8fb27904faeeb3535cbc7118f
Jordan Gardner <jordanthomasg@gmail.com>
2018-06-11 21:25:12 -0600

    Add FreePlayViewController and menu buttons

---
+++ TappySwipey/ViewControllers/FreePlayViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FreePlayViewController.swift
+//  TappySwipey
+//
+//  Created by Jordan Gardner on 6/11/18.
+//  Copyright © 2018 jordanthomasg. All rights reserved.
+//
+
+import UIKit
+
+class FreePlayViewController: BaseGameViewController {
+
+    override func updateTopLabel() {
+        self.topLabel.text = "Free Play"
+    }
+}
